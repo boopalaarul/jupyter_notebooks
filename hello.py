b2f33ca3d73fd66c022109230f41eb1dcf809759
smeeren <boopalaarul@yahoo.com>
2021-03-31 18:56:26 -0700

    added two previous projects to new repository

---
+++ hello.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2 as cv2
+
+#print("\nHello World\n") this is how easy it is to print; no classes, no mains, only the methods you def()
+
+frame1 = cv2.imread("boxplot5.png")
+#gray1 = cv2.cvtColor(frame1, op.COLOR_BGR2GRAY)
+
+#cv.absdiff(image1, image2) 
+#cv.imshow
+#cv.waitKey
+#cv.threshold (convert all below to black, above to white; grayscale presumed)
+#absolute difference of 2 photos, returns grayscale image w/ the sites of difference between the two pictures
